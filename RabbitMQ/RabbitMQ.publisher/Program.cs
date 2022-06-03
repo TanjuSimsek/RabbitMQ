c98e294684b33f3c92473f3b4b0634a49e270be8
TanjuSimsek <tanju.simsek@yandex.com>
2022-06-03 21:32:12 +0300

    basic-consumer-publisher

--- RabbitMQ/RabbitMQ.publisher/Program.cs
+++ RabbitMQ/RabbitMQ.publisher/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reflection;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace RabbitMQ.publisher
 {
@@ -6,7 +9,24 @@ namespace RabbitMQ.publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://crbdixlg:g9QiD8b2xSqzb7Ht0lZMocnmE_2XdT97@jaguar.rmq.cloudamqp.com/crbdixlg");
+
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            string message = "Hello World";
+
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "hello-queue",null,messageBody);
+
+            Console.WriteLine("Mesaj Gönderildi");
+            Console.ReadLine();
         }
     }
 }
--- RabbitMQ/RabbitMQ.subscriber/Program.cs
+++ RabbitMQ/RabbitMQ.subscriber/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMQ.subscriber
 {
@@ -6,7 +9,34 @@ namespace RabbitMQ.subscriber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://crbdixlg:g9QiD8b2xSqzb7Ht0lZMocnmE_2XdT97@jaguar.rmq.cloudamqp.com/crbdixlg");
+
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false); publisher tarafında kuyruk yoksa oluşturur
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj : " + message);
+            };
+
+
+
+
+            Console.ReadLine();
         }
+
+        
     }
 }
