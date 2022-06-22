ae5aed0ba23621508407be16c95198dd8d55c3c7
TanjuSimsek <tanju.simsek@yandex.com>
2022-06-22 21:00:07 +0300

    get-files

--- RabbitMQ/RaabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
+++ RabbitMQ/RaabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
@@ -4,15 +4,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.EntityFrameworkCore;
+using RaabbitMQWeb.ExcelCreate.Models;
 
 namespace RaabbitMQWeb.ExcelCreate.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManger;
+
+        public ProductController(UserManager<IdentityUser> userManger, AppDbContext context)
+        {
+            _userManger = userManger;
+            _context = context;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> CreateProductExcel()
+        {
+            var user =await _userManger.FindByNameAsync(User.Identity.Name);
+
+            var fileName = $"product-excel-{Guid.NewGuid().ToString().Substring(1, 10)}";
+
+            UserFile userfile = new()
+            {
+                UserId = user.Id,
+                FileName = fileName,
+                FileStatus = FileStatus.Creating
+            };
+
+           await _context.UserFiles.AddAsync(userfile);
+           await _context.SaveChangesAsync();
+           //rabbitMQ mesaj gonderimi
+           TempData["StartCreatingExcel"] = true;
+           return RedirectToAction(nameof(Files));
+
+
+        }
+
+        public async Task<IActionResult> Files()
+        {
+            var user = await _userManger.FindByNameAsync(User.Identity.Name);
+            return View(await _context.UserFiles.Where(x => x.UserId == user.Id).OrderByDescending(x => x.Id).ToListAsync());
+        }
+
     }
 }
